9433648112af5a609b753ebaa713d2577bfd1b34
Kinfe123 <kinfishtech@gmail.com>
2025-02-09 16:06:10 +0300

    tests on client app/web

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,8 +1,22 @@
 import { hook, callHook, createBefter } from "@farming-labs/befter";
 const hooks = createBefter();
 export async function getStatus() {
-  let { currHook: hookLists } = hook(hooks, "hook1", () => {
+  let {
+    currHook: hookLists,
+    afterMeta: af,
+    beforeMeta: bf,
+  } = hook(hooks, "hook1", () => {
     console.log("Excuting the main hook.");
+  });
+  const [currBf, addBf] = bf({
+    runner: "serial",
+  });
+  addBf(() => {
+    console.log("Do the preprocessing.");
+  });
+  const [currAf, addAf] = af();
+  addAf(() => {
+    console.log("Do the postprocessing.");
   });
   await callHook(hooks, "hook1");
   return { status: "ok" };
