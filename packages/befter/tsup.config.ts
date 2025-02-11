34546336af3c23673ad0590946ad47dea25f060e
Kinfe123 <kinfishtech@gmail.com>
2025-02-11 05:46:17 +0300

    biome and dockr

--- packages/befter/tsup.config.ts
+++ packages/befter/tsup.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "tsup";
 export default defineConfig({
-  clean: true,
-  dts: true,
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  sourcemap: true,
-  minify: true,
-  target: "esnext",
-  outDir: "dist",
+	clean: true,
+	dts: true,
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	sourcemap: true,
+	minify: true,
+	target: "esnext",
+	outDir: "dist",
 });
-
