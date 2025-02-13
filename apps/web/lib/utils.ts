b7d9ebbc8de3edce71407b81baa5a78e5335323d
Kinfe123 <kinfishtech@gmail.com>
2025-02-13 16:05:09 +0300

    clean up the hook before and after

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,32 +1,28 @@
 import { callHook, createBefter, hook } from "@farming-labs/befter";
 const hooks = createBefter({
-	storage: {
-		type: "local",
-	},
+  storage: {
+    type: "local",
+  },
 });
 export async function getStatus() {
-	const {
-		currHook: hookLists,
-		afterMeta: af,
-		beforeMeta: bf,
-	} = hook(hooks, "hook1", () => {
-		console.log("Excuting the main hook.");
-	});
-	const [currBf, addBf] = bf({
-		runner: "parallel",
-	});
-	const func1 = () => {
-		console.log("Do the preprocessing 1.");
-	};
-	const func2 = () => {
-		console.log("Do the preprocessing 2.");
-	};
-	addBf([func1, func2]);
+  const {
+    hooks: { currHook: hookLists, afterMeta: af, beforeMeta: bf },
+  } = hook(hooks, "hook1", () => {
+    console.log("Excuting the main hook.");
+  });
+  const [currBf, addBf] = bf({ runner: "serial" });
+  const func1 = () => {
+    console.log("Do the preprocessing 1.");
+  };
+  const func2 = () => {
+    console.log("Do the preprocessing 2.");
+  };
+  addBf([func1, func2]);
 
-	const [currAf, addAf] = af();
-	addAf(() => {
-		console.log("Do the postprocessing.");
-	});
-	await callHook(hooks, "hook1");
-	return { status: "ok" };
+  const [currAf, addAf] = af();
+  addAf(() => {
+    console.log("Do the postprocessing.");
+  });
+  await callHook(hooks, "hook1");
+  return { status: "ok" };
 }
